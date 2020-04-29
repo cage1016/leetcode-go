9c1ffa0771fa310a0d335263d9935d428f90ae99
Kai-Chu Chung <cage.chung@gmail.com>
2020-04-30 01:27:10 +0800

    104

---
+++ 104.maximum-depth-of-binary-tree/104.maximum-depth-of-binary-tree.go
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=104 lang=golang
+ *
+ * [104] Maximum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (64.78%)
+ * Likes:    2186
+ * Dislikes: 66
+ * Total Accepted:    754.5K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, find its maximum depth.
+ * 
+ * The maximum depth is the number of nodes along the longest path from the
+ * root node down to the farthest leaf node.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * return its depth = 3.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	} else {
+		left := maxDepth(root.Left)
+		right := maxDepth(root.Right)
+		return int(math.Max(float64(left), float64(right))) + 1
+	}
+}
+// @lc code=end
+
