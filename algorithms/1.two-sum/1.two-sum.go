33815c5bf20d40d364196f410afb6a0d21820668
Kai-Chu Chung <cage.chung@gmail.com>
2020-04-30 16:18:04 +0800

    update folder structure

--- algorithms/1.two-sum/1.two-sum.go
+++ algorithms/1.two-sum/1.two-sum.go
@@ -1,3 +1,5 @@
+package twosum
+
 /*
  * @lc app=leetcode id=1 lang=golang
  *
@@ -15,32 +17,32 @@
  *
  * Given an array of integers, return indices of the two numbers such that they
  * add up to a specific target.
- * 
+ *
  * You may assume that each input would have exactly one solution, and you may
  * not use the same element twice.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Given nums = [2, 7, 11, 15], target = 9,
- * 
+ *
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
 func twoSum(nums []int, target int) []int {
 	m := map[int]int{}
 	for i := 0; i < len(nums); i++ {
-		x := nums[i];
-		if _, ok := m[target - x]; ok {
-			return []int{m[target - x], i }
-		}		
-		m[x] = i;
+		x := nums[i]
+		if _, ok := m[target-x]; ok {
+			return []int{m[target-x], i}
+		}
+		m[x] = i
 	}
 	return []int{}
 }
-// @lc code=end
 
+// @lc code=end
